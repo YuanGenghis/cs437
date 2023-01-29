cdefd2e2aa8ecfb47b619f4b5173e7b509c3e938
RogerQi <rizhaoq2@illinois.edu>
2023-01-29 17:53:15 -0600

    add comment to lab 1.a ckpt

--- script.py
+++ script.py
@@ -5,15 +5,26 @@ speed = 10
 
 def main():
     while True:
+        # 37 is the baseline distance; which seems to be 37cm
         scan_list = fc.scan_step(37)
         if not scan_list:
             continue
 
         tmp = scan_list[3:7]
         print(tmp)
-        if tmp != [2,2,2,2] and tmp != [0,0]:
+
+        # 3 intervals of ultrasonic sensor here
+        # Case 1: dist < 10cm           -> 0
+        # Case 2: 10cm < dist < 37cm    -> 1
+        # Case 3: 37cm < dist           -> 2
+
+        # The check for len(tmp) handles initializations
+        if tmp != [2,2,2,2] and len(tmp) == 4:
+            # When runs into an obstacle, the car will back up for 0.3s
             fc.backward(speed)
             time.sleep(0.3)
+
+            # Then turn right for 0.1s
             fc.turn_right(speed)
             time.sleep(0.1)
         else:
